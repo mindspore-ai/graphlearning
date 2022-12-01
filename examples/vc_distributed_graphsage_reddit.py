26daf25ae5af8a90cdf738053d6ecb8b3945a3fe
fengxun705612 <fengxun5@huawei.com>
2022-12-01 19:03:54 +0800

    1
    1
    
    4
    
    5

--- examples/vc_distributed_graphsage_reddit.py
+++ examples/vc_distributed_graphsage_reddit.py
@@ -26,10 +26,11 @@ import mindspore.nn as nn
 import mindspore.context as context
 from mindspore.communication import init, get_rank, get_group_size
 from mindspore.nn import Cell
+import mindspore.dataset as ds
+from mindspore import ops
 
 from mindspore_gl.dataset.reddit import Reddit
 from mindspore_gl.dataloader.samplers import RandomBatchSampler, DistributeRandomBatchSampler
-from mindspore_gl.dataloader.dataloader import DataLoader
 
 sys.path.append(os.path.join(os.getcwd(), "..", "model_zoo"))
 # pylint: disable=C0413
@@ -67,49 +68,50 @@ def main():
     train_sampler = DistributeRandomBatchSampler(rank_id, world_size, data_source=graph_dataset.train_nodes,
                                                  batch_size=args.batch_size)
     test_sampler = RandomBatchSampler(data_source=graph_dataset.test_nodes, batch_size=args.batch_size)
-    dataset = GraphSAGEDataset(graph_dataset, [25, 10], args.batch_size, single_size=single_size)
-    train_dataloader = DataLoader(dataset, sampler=train_sampler, num_workers=1)
-    test_dataloader = DataLoader(dataset, sampler=test_sampler, num_workers=0)
-    print(train_dataloader.__len__())
-    print(test_dataloader.__len__())
+    train_dataset = GraphSAGEDataset(graph_dataset, [25, 10], args.batch_size, len(list(train_sampler)))
+    test_dataset = GraphSAGEDataset(graph_dataset, [25, 10], args.batch_size, len(list(test_sampler)))
+    train_dataloader = ds.GeneratorDataset(train_dataset, ['seeds_idx', 'label', 'nid_feat', 'edges'],
+                                           sampler=train_sampler, python_multiprocessing=True)
+    test_dataloader = ds.GeneratorDataset(test_dataset, ['seeds_idx', 'label', 'nid_feat', 'edges'],
+                                          sampler=test_sampler, python_multiprocessing=True)
+    if args.profile:
+        ms_profiler = Profiler(subgraph="ALL", is_detail=True, is_show_op_path=False, output_path="./prof_result")
 
     appr_dim = math.ceil(graph_dataset.num_classes/8)*8
     model = SAGENet(graph_dataset.num_features, args.num_hidden, appr_dim, graph_dataset.num_classes)
     optimizer = nn.optim.Adam(model.trainable_params(), learning_rate=args.lr, weight_decay=args.weight_decay)
     loss = LossNet(model)
-    train_net = nn.TrainOneStepCell(loss, optimizer)
-    if args.profile:
-        ms_profiler = Profiler(subgraph="ALL", is_detail=True, is_show_op_path=False, output_path="./prof_result")
+    grad_fn = ops.value_and_grad(loss, None, optimizer.parameters, has_aux=False)
+
+    def train_one_step(data):
+        seeds_idx, label, nid_feat, edges = data
+        n_nodes = nid_feat.shape[0]
+        n_edges = edges.shape[1]
+        loss, grads = grad_fn(seeds_idx, nid_feat, label, edges, n_nodes, n_edges)
+        loss = ops.depend(loss, optimizer(grads))
+        return loss
+
     for epoch in range(args.epochs):
         start = time.time()
-        train_net.set_train(True)
+        model.set_train(True)
         for iter_num, data in enumerate(train_dataloader):
-            seeds_idx, label, nid_feat, edges = data['seeds_ids'], data['label'], data['feat'], data['pad_sample_edges']
-            n_nodes = nid_feat.shape[0]
-            n_edges = edges.shape[1]
-            seeds_idx = ms.Tensor.from_numpy(seeds_idx)
-            nid_feat = ms.Tensor.from_numpy(nid_feat)
-            nid_label = ms.Tensor.from_numpy(label)
-            edges = ms.Tensor.from_numpy(edges)
-            train_loss = train_net(seeds_idx, nid_feat, nid_label, edges, n_nodes, n_edges)
+            train_loss = train_one_step(data)
             if iter_num % 10 == 0:
                 print(f"rank_id:{rank_id} Iteration/Epoch: {iter_num}:{epoch} train loss: {train_loss}", flush=True)
         end = time.time()
         epoch_time = end - start
         print(f"rank_id:{rank_id} Epoch/Time: {epoch}:{epoch_time}", flush=True)
 
-        train_net.set_train(False)
+        model.set_train(False)
         total_prediction = 0
         correct_prediction = 0
         for iter_num, data in enumerate(test_dataloader):
-            seeds_idx, label, nid_feat, edges = data['seeds_ids'], data['label'], data['feat'], data['pad_sample_edges']
+            seeds_idx, label, nid_feat, edges = data
             n_nodes = nid_feat.shape[0]
             n_edges = edges.shape[1]
-            nid_feat = ms.Tensor.from_numpy(nid_feat)
-            edges = ms.Tensor.from_numpy(edges)
-
             out = model(nid_feat, edges, n_nodes, n_edges)
             out = out.asnumpy()
+            seeds_idx = seeds_idx.asnumpy()
             predict = np.argmax(out[seeds_idx], axis=1)
             correct_prediction += len(np.nonzero(np.equal(predict, label))[0])
             total_prediction += len(seeds_idx)
