6af81f5687e8c3a29302786fa14ff9d41d15fba7
fengxun705612 <fengxun5@huawei.com>
2022-12-19 17:11:37 +0800

    1
    2
    
    4
    
    4

--- mindspore_gl/dataset/imdb_binary.py
+++ mindspore_gl/dataset/imdb_binary.py
@@ -14,8 +14,12 @@
 # ============================================================================
 """IMDBBinary"""
 #pylint: disable=W0702
+import random
 from typing import Union
+import os
 import os.path as osp
+import urllib.request
+import zipfile
 import numpy as np
 from mindspore_gl.graph.graph import MindHomoGraph
 
@@ -23,7 +27,7 @@ from mindspore_gl.graph.graph import MindHomoGraph
 class IMDBBinary:
 
     """
-    IMDBBinary  Dataset, a source dataset for reading and parsing IMDBBinary  dataset.
+    IMDBBinary Dataset, a source dataset for reading and parsing IMDBBinary dataset.
 
     Args:
         root(str): path to the root directory that contains imdb_binary_with_mask.npz
@@ -37,8 +41,36 @@ class IMDBBinary:
         >>> root = "path/to/imdb_binary"
         >>> dataset = IMDBBinary(root)
 
-    """
+    About IMDBBinary dataset:
 
+    IMDBBinary Dataset, a source dataset for reading and parsing IMDBBinary  dataset. IMDB-BINARY is a movie
+    collaboration dataset that consists of the ego-networks of 1,000 actors/actresses who played roles in movies
+    in IMDB. In each graph, nodes represent actors/actress, and there is an edge between them if they appear in the
+    same movie. These graphs are derived from the Action and Romance genres.
+
+    Statistics:
+
+    - Nodes: 19773
+    - Edges: 193062
+    - Number of Graphs： 1000
+    - Number of Classes: 2
+    - Label split:
+
+      - Train: 800
+      - Valid: 200
+
+    Dataset can be download here: <https://ls11-www.cs.tu-dortmund.de/people/morris/graphkerneldatasets/IMDB-BINARY.zip>
+    You can organize the dataset files into the following directory structure and read by `process` API.
+
+    .. code-block::
+
+        .
+        ├── IMDB-BINARY_A.txt
+        ├── IMDB-BINARY_graph_indicator.txt
+        └── IMDB-BINARY_graph_labels.txt
+
+    """
+    url = 'https://ls11-www.cs.tu-dortmund.de/people/morris/graphkerneldatasets/IMDB-BINARY.zip'
     def __init__(self, root):
         if not isinstance(root, str):
             raise TypeError(f"For '{self.cls_name}', the 'root' should be a str, "
@@ -58,88 +90,258 @@ class IMDBBinary:
         self._val_mask = None
         self._test_mask = None
 
-        self.load()
+        if osp.exists(self._path):
+            self.load()
+        else:
+            path, file_name = self._download(self._root)
+            self.process(path, file_name)
+            self.load()
+
+    def _download(self, save_dir):
+        """Download dataset"""
+        file = self.url.rpartition('/')[-1]
+        path = osp.join(save_dir, file)
+        unzip_name = file.rpartition('.')[0]
+        unzip_path = osp.join(save_dir, unzip_name)
+        if os.path.exists(unzip_path):
+            return unzip_path, unzip_name
+        data = urllib.request.urlopen(self.url)
+        with open(path, 'wb') as f:
+            while True:
+                chunk = data.read(10*1024*1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+        with zipfile.ZipFile(path, 'r') as f:
+            f.extractall(save_dir)
+        os.remove(path)
+        return unzip_path, unzip_name
+
+    def process(self, path, file_name):
+        """Process data"""
+        label_file_name = file_name+'_graph_labels.txt'
+        label_path = osp.join(path, label_file_name)
+        self._graph_label = np.loadtxt(label_path)
+
+        indicator_file_name = file_name+'_graph_indicator.txt'
+        indicator_path = osp.join(path, indicator_file_name)
+        graph_per_nodes = np.loadtxt(indicator_path, dtype=int)
+        num_nodes = len(graph_per_nodes)
+        self._graph_nodes = np.bincount(graph_per_nodes).cumsum().tolist()
+        self._node_feat = np.zeros((num_nodes, 136))
+        edges_file_name = file_name + '_A.txt'
+        edges_path = osp.join(path, edges_file_name)
+        load_edges = np.loadtxt(edges_path, delimiter=',', dtype=[('src', int), ('dst', int)])
+        start = 0
+        self._graph_edges = [0]
+        adj_coo_row, adj_coo_col = [], []
+        for i, node_count in enumerate(self._graph_nodes[1:]):
+            for idx in range(start, len(load_edges)):
+                if load_edges[idx][0] > node_count:
+                    break
+                elif idx == len(load_edges) - 1:
+                    idx += 1
+                    break
+            adj_list = load_edges[start: idx].tolist()
+            adj_list = list(set(adj_list))
+            adj_list = sorted(adj_list, key=lambda x: [x[0], x[1]])
+            src = [x[0] - 1 for x in adj_list]
+            tag = [x[1] - 1 for x in adj_list]
+            adj_coo_col += src
+            adj_coo_row += tag
+            last_edge = self._graph_edges[-1]
+            self._graph_edges.append(last_edge + len(adj_list))
+            start = idx
+        mask_idx = list(range(len(self._graph_label)))
+        random.shuffle(mask_idx)
+        train_mask = [0] * len(mask_idx)
+        for idx in mask_idx[len(mask_idx) // 10:]:
+            train_mask[idx] = 1
+        val_mask = [0] * len(mask_idx)
+        for idx in mask_idx[:len(mask_idx) // 10]:
+            val_mask[idx] = 1
+        edge_array = np.array([adj_coo_col, adj_coo_row])
+
+        for i in range(1, len(self._graph_nodes)):
+            start = self._graph_nodes[i - 1]
+            end = self._graph_nodes[i]
+            for j in range(start, end):
+                self._node_feat[j, j - start] = 1
+
+        np.savez(self._path, edge_array=edge_array, train_mask=train_mask, val_mask=val_mask,
+                 node_feat=self._node_feat, graph_label=self._graph_label,
+                 graph_edges=self._graph_edges, graph_nodes=self._graph_nodes)
 
     def load(self):
+        """Load the saved npz dataset from files."""
         self._npz_file = np.load(self._path)
         self._edge_array = self._npz_file['edge_array'].astype(np.int32)
         self._graph_edges = self._npz_file['graph_edges'].astype(np.int32)
-
+        self._graph_nodes = self._npz_file['graph_nodes'].astype(np.int32)
         self._graphs = np.array(list(range(len(self._graph_edges))))
 
     @property
     def num_features(self):
+        """
+        Feature size of each node
+
+        Returns:
+            int, the number of feature size
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> num_features = dataset.num_features
+        """
         return self.node_feat.shape[-1]
 
     @property
     def num_edge_features(self):
+        """
+        Feature size of each edge
+
+        Returns:
+            int, the number of feature size
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> num_edge_features = dataset.num_edge_features
+        """
         return 0
 
     @property
     def num_classes(self):
+        """
+        Number of label classes
+
+        Returns:
+            int, the number of classes
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> num_classes = dataset.num_classes
+        """
         return len(np.unique(self.graph_label))
 
     @property
     def train_mask(self):
+        """
+        Mask of training nodes
+
+        Returns:
+            numpy.ndarray, array of mask
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> train_mask = dataset.train_mask
+        """
         if self._train_mask is None:
             self._train_mask = self._npz_file['train_mask']
         return self._train_mask
 
     @property
-    def test_mask(self):
-        if self._test_mask is None:
-            self._test_mask = self._npz_file['test_mask']
-        return self._test_mask
-
-    @property
     def val_mask(self):
+        """
+        Mask of validation nodes
+
+        Returns:
+            numpy.ndarray, array of mask
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> val_mask = dataset.val_mask
+        """
         if self._val_mask is None:
             self._val_mask = self._npz_file['val_mask']
         return self._val_mask
 
     @property
     def graph_nodes(self):
-        """return graph nodes"""
-        try:
-            if self._graph_nodes is None:
-                self._graph_nodes = self._npz_file['graph_nodes'].astype(np.int32)
-            return self._graph_nodes
-        except:
-            ############################################################################
-            # NPZ File Is Known To Be Bugged When Sharing NPZFileHandler Across Process
-            # Checkout This Issue: https://github.com/numpy/numpy/issues/18124
-            #############################################################################
-            self.load()
-            self._graph_nodes = self._npz_file['graph_nodes'].astype(np.int32)
-            return self._graph_nodes
+        """
+        Accumulative graph nodes count
+
+        Returns:
+            numpy.ndarray, array of accumulative nodes
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> val_mask = dataset.graph_nodes
+        """
+        if self._graph_nodes is None:
+            self._graph_nodes = self._npz_file['graph_nodes']
+        return self._graph_nodes
 
     @property
     def graph_edges(self):
+        """
+        Accumulative graph edges count
+
+        Returns:
+            numpy.ndarray, array of accumulative edges
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> val_mask = dataset.graph_edges
+        """
         if self._graph_edges is None:
             self._graph_edges = self._npz_file['graph_edges'].astype(np.int32)
         return self._graph_edges
 
     @property
     def train_graphs(self):
+        """
+        Train graph id
+
+        Returns:
+            numpy.ndarray, array of train graph id
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> train_graphs = dataset.train_graphs
+        """
         return (np.nonzero(self.train_mask)[0]).astype(np.int32)
 
     @property
     def val_graphs(self):
+        """
+        Valid graph id
+
+        Returns:
+            numpy.ndarray, array of valid graph id
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> val_graphs = dataset.val_graphs
+        """
         return (np.nonzero(self.val_mask)[0]).astype(np.int32)
 
     @property
-    def test_graphs(self):
-        return (np.nonzero(self.test_mask)[0]).astype(np.int32)
-
-    @property
     def graph_count(self):
-        return len(self._graphs)
+        """
+        Total graph numbers
+
+        Returns:
+            int, numbers of graph
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> graph_count = dataset.graph_count
+        """
+        return len(self.graph_label)
 
     @property
     def node_feat(self):
+        """
+        Node features
+
+        Returns:
+            numpy.ndarray, array of node feature
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> node_feat = dataset.node_feat
+        """
         if self._node_feat is None:
             self._node_feat = self._npz_file["node_feat"]
-
         return self._node_feat
 
     def graph_feat(self, graph_idx):
@@ -147,6 +349,16 @@ class IMDBBinary:
 
     @property
     def graph_label(self):
+        """
+        Graph label
+
+        Returns:
+            numpy.ndarray, array of graph label
+
+        Examples:
+            >>> #dataset is an instance object of Dataset
+            >>> graph_label = dataset.graph_label
+       """
         if self._graph_label is None:
             self._graph_label = self._npz_file["graph_label"]
         return self._graph_label.astype(np.int32)
--- tests/st/test_gin.py
+++ tests/st/test_gin.py
@@ -12,322 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-""" test gin """
-import numpy as np
+""" test geniepath """
+import os
+import shutil
 import pytest
-import mindspore as ms
-import mindspore.nn as nn
-import mindspore.ops as ops
-import mindspore.context as context
-
-from mindspore_gl.nn import GINConv
-from mindspore_gl.nn import GNNCell
-from mindspore_gl.dataloader.dataset import Dataset
-from mindspore_gl.dataloader.dataloader import DataLoader
-from mindspore_gl.dataloader.samplers import RandomBatchSampler
-from mindspore_gl.dataset.imdb_binary import IMDBBinary
-from mindspore_gl import BatchedGraph, BatchedGraphField
-from mindspore_gl.graph.ops import BatchHomoGraph, PadArray2d, PadHomoGraph, PadMode, PadDirection
-
-data_path = "/home/workspace/mindspore_dataset/GNN_Dataset/"
-
-
-class ApplyNodeFunc(nn.Cell):
-    """
-    Update the node feature hv with MLP, BN and ReLU.
-    """
-
-    def __init__(self, mlp):
-        super(ApplyNodeFunc, self).__init__()
-        self.mlp = mlp
-        self.bn = nn.BatchNorm1d(self.mlp.output_dim)
-
-    def construct(self, h):
-        h = self.mlp(h)
-        h = self.bn(h)
-        h = nn.ReLU()(h)
-        return h
-
-
-class MLP(nn.Cell):
-    """MLP"""
-
-    def __init__(self, num_layers, input_dim, hidden_dim, output_dim):
-        """
-        num_layers: number of layers in the neural networks (EXCLUDING the input layer).
-            If num_layers=1, this reduces to linear model.
-        input_dim: dimensionality of input features
-        hidden_dim: dimensionality of hidden units at ALL layers
-        output_dim: number of classes for prediction
-        """
-        super().__init__()
-        self.num_layers = num_layers
-        self.output_dim = output_dim
-        if num_layers < 1:
-            raise ValueError("number of layers should be positive!")
-        if num_layers == 1:
-            # Linear model
-            self.linear = nn.Dense(input_dim, output_dim)
-        else:
-            # Multi-layer model
-            self.linears = nn.CellList()
-            self.batch_norms = nn.CellList()
-
-            self.linears.append(nn.Dense(input_dim, hidden_dim))
-            for _ in range(num_layers - 2):
-                self.linears.append(nn.Dense(hidden_dim, hidden_dim))
-            self.linears.append(nn.Dense(hidden_dim, output_dim))
-
-            for _ in range(num_layers - 1):
-                self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
-
-    def construct(self, x):
-        if self.num_layers == 1:
-            return self.linear(x)
-        # If MLP
-        h = x
-        for layer in range(self.num_layers - 1):
-            h = ms.ops.ReLU()(self.batch_norms[layer](self.linears[layer](h)))
-        return self.linears[self.num_layers - 1](h)
-
-
-class GinNet(GNNCell):
-    """GinNet"""
-
-    def __init__(self,
-                 num_layers,
-                 num_mlp_layers,
-                 input_dim,
-                 hidden_dim,
-                 output_dim,
-                 final_dropout=0.1,
-                 learn_eps=False,
-                 graph_pooling_type='sum',
-                 neighbor_pooling_type='sum'
-                 ):
-        super().__init__()
-        self.final_dropout = final_dropout
-        self.num_layers = num_layers
-        self.graph_pooling_type = graph_pooling_type
-        self.neighbor_pooling_type = neighbor_pooling_type
-        self.learn_eps = learn_eps
-
-        self.mlps = nn.CellList()
-        self.convs = nn.CellList()
-        self.batch_norms = nn.CellList()
-
-        if self.graph_pooling_type not in ("sum", "avg"):
-            raise SyntaxError("Graph pooling type not supported yet.")
-        for layer in range(num_layers - 1):
-            if layer == 0:
-                self.mlps.append(MLP(num_mlp_layers, input_dim, hidden_dim, hidden_dim))
-            else:
-                self.mlps.append(MLP(num_mlp_layers, hidden_dim, hidden_dim, hidden_dim))
-            self.convs.append(GINConv(ApplyNodeFunc(self.mlps[layer]), learn_eps=self.learn_eps,
-                                      aggregation_type=self.neighbor_pooling_type))
-            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
-
-        self.linears_prediction = nn.CellList()
-        for layer in range(num_layers):
-            if layer == 0:
-                self.linears_prediction.append(nn.Dense(input_dim, output_dim))
-            else:
-                self.linears_prediction.append(nn.Dense(hidden_dim, output_dim))
-
-    def construct(self, x, edge_weight, g: BatchedGraph):
-        """GinNet forward"""
-        hidden_rep = [x]
-        h = x
-        for layer in range(self.num_layers - 1):
-            h = self.convs[layer](h, edge_weight, g)
-            h = self.batch_norms[layer](h)
-            h = nn.ReLU()(h)
-            hidden_rep.append(h)
-
-        score_over_layer = 0
-        for layer, h in enumerate(hidden_rep):
-            if self.graph_pooling_type == 'sum':
-                pooled_h = g.sum_nodes(h)
-            else:
-                pooled_h = g.avg_nodes(h)
-            score_over_layer = score_over_layer + nn.Dropout(self.final_dropout)(
-                self.linears_prediction[layer](pooled_h))
-
-        return score_over_layer
-
-
-class LossNet(GNNCell):
-    """
-    LossNet definition
-    """
-
-    def __init__(self, net):
-        super().__init__()
-        self.net = net
-        self.loss_fn = nn.loss.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='none')
-
-    def construct(self, node_feat, edge_weight, target, g: BatchedGraph):
-        predict = self.net(node_feat, edge_weight, g)
-        target = ops.Squeeze()(target)
-        loss = self.loss_fn(predict, target)
-        ##############################
-        # Mask Loss
-        ##############################
-        return ms.ops.ReduceSum()(loss * g.graph_mask)
-
-
-class MultiHomoGraphDataset(Dataset):
-    """MultiHomoGraphDataset"""
-
-    def __init__(self, dataset, batch_size, mode=PadMode.CONST, node_size=1500, edge_size=15000):
-        self._dataset = dataset
-        self._batch_size = batch_size
-        self.batch_fn = BatchHomoGraph()
-        self.batched_edge_feat = None
-        if mode == PadMode.CONST:
-            self.node_feat_pad_op = PadArray2d(dtype=np.float32, mode=PadMode.CONST, direction=PadDirection.COL,
-                                               size=(1500, dataset.num_features), fill_value=0)
-            self.edge_feat_pad_op = PadArray2d(dtype=np.float32, mode=PadMode.CONST, direction=PadDirection.COL,
-                                               size=(edge_size, dataset.num_edge_features), fill_value=0)
-            self.graph_pad_op = PadHomoGraph(n_edge=edge_size, n_node=node_size, mode=PadMode.CONST)
-        else:
-            self.node_feat_pad_op = PadArray2d(dtype=np.float32, mode=PadMode.AUTO, direction=PadDirection.COL,
-                                               fill_value=0)
-            self.edge_feat_pad_op = PadArray2d(dtype=np.float32, mode=PadMode.AUTO, direction=PadDirection.COL,
-                                               fill_value=0)
-            self.graph_pad_op = PadHomoGraph(mode=PadMode.AUTO)
-        ################
-        # For Padding
-        ################
-        self.train_mask = np.array([True] * (self._batch_size + 1))
-        self.train_mask[-1] = False
-
-    def __getitem__(self, batch_graph_idx):
-        graph_list = []
-        feature_list = []
-        for idx in range(batch_graph_idx.shape[0]):
-            graph_list.append(self._dataset[batch_graph_idx[idx]])
-            feature_list.append(self._dataset.graph_feat(batch_graph_idx[idx]))
-        #########################
-        # Batch Graph
-        ########################
-        batch_graph = self.batch_fn(graph_list)
-        #########################
-        # Pad Graph
-        ########################
-        batch_graph = self.graph_pad_op(batch_graph)
-        #########################
-        # Batch Node Feat
-        ########################
-        batched_node_feat = np.concatenate(feature_list)
-        ###################
-        # Pad NodeFeat
-        ##################
-        batched_node_feat = self.node_feat_pad_op(batched_node_feat)
-        batched_label = self._dataset.graph_label[batch_graph_idx]
-        ######################
-        # Pad Label
-        #####################
-        batched_label = np.append(batched_label, batched_label[-1] * 0)
-        #################################
-        # Get Edge Feat
-        #################################
-        if self.batched_edge_feat is None or self.batched_edge_feat.shape[0] < batch_graph.edge_count:
-            del self.batched_edge_feat
-            self.batched_edge_feat = np.ones([batch_graph.edge_count, 1], dtype=np.float32)
-
-        ##############################################################################
-        # Trigger Node_Map_Idx/Edge_Map_Idx Computation, Because It Is Lazily Computed
-        ###############################################################################
-        _ = batch_graph.batch_meta.node_map_idx
-        _ = batch_graph.batch_meta.edge_map_idx
-
-        return batch_graph, batched_label, batched_node_feat, self.batched_edge_feat[:batch_graph.edge_count, :]
-
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
+
 def test_gin():
-    """ test gin """
-    batch_size = 32
-    context.set_context(device_target="GPU", mode=context.GRAPH_MODE, enable_graph_kernel=True)
+    """
+    Features: gin
+    Description: Test gin with imdb
+    Expectation: The output is as expected.
+    """
+    if not os.path.exists('./ci_temp'):
+        os.mkdir('ci_temp')
+    if os.path.exists('ci_temp/gin'):
+        shutil.rmtree('ci_temp/gin')
 
-    dataset = IMDBBinary(data_path)
-    train_batch_sampler = RandomBatchSampler(dataset.train_graphs, batch_size=batch_size)
-    multi_graph_dataset = MultiHomoGraphDataset(dataset, batch_size)
-    train_dataloader = DataLoader(dataset=multi_graph_dataset, sampler=train_batch_sampler, num_workers=4,
-                                  persistent_workers=True, prefetch_factor=4)
+    cmd_copy = "cp -r ../../model_zoo/gin/ ./ci_temp/"
+    os.system(cmd_copy)
 
-    test_batch_sampler = RandomBatchSampler(dataset.val_graphs, batch_size=batch_size)
-    test_dataloader = DataLoader(dataset=multi_graph_dataset, sampler=test_batch_sampler, num_workers=0,
-                                 persistent_workers=False)
+    cmd_train = "python ./ci_temp/gin/trainval_imdb_binary.py --epochs 10 " \
+                "--data_path=\"/home/workspace/mindspore_dataset/GNN_Dataset/\" >> " \
+                "./ci_temp/gin/trainval_imdb_binary.log"
+    os.system(cmd_train)
 
-    np_graph_mask = [1] * (batch_size + 1)
-    np_graph_mask[-1] = 0
-    constant_graph_mask = ms.Tensor(np_graph_mask, dtype=ms.int32)
-
-    net = GinNet(num_layers=5,
-                 num_mlp_layers=2,
-                 input_dim=dataset.num_features,
-                 hidden_dim=64,
-                 output_dim=dataset.num_classes,
-                 final_dropout=0.5,
-                 learn_eps=True,
-                 graph_pooling_type='sum',
-                 neighbor_pooling_type='sum')
-
-    learning_rates = nn.piecewise_constant_lr(
-        [50, 100, 150, 200, 250, 300, 350], [0.01, 0.005, 0.0025, 0.00125, 0.000625, 0.0003125, 0.00015625])
-    optimizer = nn.optim.Adam(net.trainable_params(), learning_rate=learning_rates)
-    loss = LossNet(net)
-    train_net = nn.TrainOneStepCell(loss, optimizer)
-
-    best_acc = 0
-    for _ in range(50):
-        train_net.set_train(True)
-        total_iter = 0
-        while True:
-            for data in train_dataloader:
-                batch_graph, label, node_feat, edge_feat = data
-                node_feat = ms.Tensor.from_numpy(node_feat)
-                edge_feat = ms.Tensor.from_numpy(edge_feat)
-                label = ms.Tensor.from_numpy(label)
-                batch_homo = BatchedGraphField(
-                    ms.Tensor.from_numpy(batch_graph.adj_coo[0]),
-                    ms.Tensor.from_numpy(batch_graph.adj_coo[1]),
-                    batch_graph.node_count,
-                    batch_graph.edge_count,
-                    ms.Tensor.from_numpy(batch_graph.batch_meta.node_map_idx),
-                    ms.Tensor.from_numpy(batch_graph.batch_meta.edge_map_idx),
-                    constant_graph_mask
-                )
-                train_net(node_feat, edge_feat, label, *batch_homo.get_batched_graph())
-                total_iter += 1
-                if total_iter == 50:
-                    break
-            if total_iter == 50:
-                break
-
-        train_net.set_train(False)
-        test_count = 0
-        for data in test_dataloader:
-            batch_graph, label, node_feat, edge_feat = data
-            node_feat = ms.Tensor.from_numpy(node_feat)
-            edge_feat = ms.Tensor.from_numpy(edge_feat)
-            batch_homo = BatchedGraphField(
-                ms.Tensor.from_numpy(batch_graph.adj_coo[0]),
-                ms.Tensor.from_numpy(batch_graph.adj_coo[1]),
-                batch_graph.node_count,
-                batch_graph.edge_count,
-                ms.Tensor.from_numpy(batch_graph.batch_meta.node_map_idx),
-                ms.Tensor.from_numpy(batch_graph.batch_meta.edge_map_idx),
-                constant_graph_mask
-            )
-            output = net(node_feat, edge_feat, *batch_homo.get_batched_graph()).asnumpy()
-            label = label
-            predict = np.argmax(output, axis=1)
-            test_count += np.sum(np.equal(predict, label) * np_graph_mask)
-        test_acc = test_count / len(test_dataloader) / batch_size
-        best_acc = test_acc if test_acc > best_acc else best_acc
-    assert best_acc > 0.63
+    file = open("./ci_temp/gin/trainval_imdb_binary.log", "r")
+    log_info = file.readlines()
+    file.close()
+    last_info = log_info[-2]
+    test_acc = float(last_info[last_info.find('Test acc '):].replace('Test acc ', '').replace('\n', ''))
+    assert test_acc > 0.6
